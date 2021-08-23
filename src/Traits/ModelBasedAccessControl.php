78627b0f02d759186be454e76e72ee94ed4e01f7
Taner <hello@taner.work>
2021-08-23 14:53:19 +1000

    `can`/`cannot` methods for model trait

--- src/Traits/ModelBasedAccessControl.php
+++ src/Traits/ModelBasedAccessControl.php
@@ -2,10 +2,16 @@
 
 namespace Tanerkay\ModelAcl\Traits;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Tanerkay\ModelAcl\ModelAclServiceProvider;
 use Tanerkay\ModelAcl\AccessControlStatus;
+use Tanerkay\ModelAcl\Models\ModelHasAccessControl;
 
+/**
+ * @property-read Collection<ModelHasAccessControl> $accessControl
+ */
 trait ModelBasedAccessControl
 {
     protected bool $disableModelBasedAccessControl = false;
@@ -24,5 +30,17 @@ trait ModelBasedAccessControl
         }
 
         return true;
+    }
+
+    public function can($ability, ?Authenticatable $user = null): bool
+    {
+        return $this->accessControl->each(function ($accessControl) {
+            // ...
+        })->every(true);
+    }
+
+    public function cannot($ability, ?Authenticatable $user = null): bool
+    {
+        return ! $this->can($ability, $user);
     }
 }
