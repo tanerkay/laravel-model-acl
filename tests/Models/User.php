0c48a53ef67d00a707d705d0e758ca5d2cd3ff1f
Taner Kucukyuruk <tanerkay@protonmail.com>
2025-03-05 11:35:03 +0100

    simplify user model in tests

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -2,10 +2,9 @@
 
 namespace Tanerkay\ModelAcl\Test\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as BaseUser;
 use Illuminate\Support\Collection;
 
 /**
@@ -13,7 +12,7 @@ use Illuminate\Support\Collection;
  * @property string $name
  * @property Collection $roles
  */
-class User extends Model implements Authenticatable
+class User extends BaseUser
 {
     protected $table = 'users';
 
@@ -22,38 +21,6 @@ class User extends Model implements Authenticatable
     protected $casts = [
         'roles' => 'collection',
     ];
-
-    public function getAuthIdentifierName(): string
-    {
-        return 'id';
-    }
-
-    public function getAuthIdentifier(): mixed
-    {
-        $name = $this->getAuthIdentifierName();
-
-        return $this->attributes[$name];
-    }
-
-    public function getAuthPassword(): string
-    {
-        return $this->attributes['password'];
-    }
-
-    public function getRememberToken(): string
-    {
-        return 'token';
-    }
-
-    public function setRememberToken($value): void
-    {
-        // ...
-    }
-
-    public function getRememberTokenName(): string
-    {
-        return 'tokenName';
-    }
 
     public function nodes(): HasMany
     {
